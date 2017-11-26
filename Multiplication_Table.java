04a9513abff9cc10b5690624a60b44915a901f03
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2017-11-26 13:41:54 -0500

    Create Multiplication_Table.java

---
+++ Multiplication_Table.java
@@ -0,0 +1,9 @@
+public class Multiplication_Table{
+	public static void main(String args[]) {
+		for (int i = 1; i < 13; i++) {
+			for (int x = 1; x < 13; x++) {
+				System.out.println(i + " x " + x + " = " + i * x);
+			}
+		}
+	}
+}
